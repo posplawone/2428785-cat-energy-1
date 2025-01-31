6619eb497289ef5b18adb32be12f527c6f443595
posplaw <flatcher98@gmail.com>
2025-01-31 15:05:45 +0700

    Доработки

--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,35 @@
-let button = document.querySelector('.main-header__toggle');
-let menu = document.querySelector('.main-header__menu');
+let button = document.querySelector(".main-header__toggle");
+let menu = document.querySelector(".main-header__menu");
 let header = document.querySelector(".main-header");
 
-button.addEventListener('click', () => {
-  if (menu.classList.contains('main-header__menu-closed') == true) {
-    menu.classList.remove('main-header__menu-closed');
-  } else {
-    menu.classList.add('main-header__menu-closed');
-  }
-})
+if (button) {
+  button.addEventListener("click", () => {
+    if (menu.classList.contains("main-header__menu-closed") == true) {
+      menu.classList.remove("main-header__menu-closed");
+    } else {
+      menu.classList.add("main-header__menu-closed");
+    }
+  });
+}
 
-header.classList.remove("main-header--nojs");
+if (header) {
+  header.classList.remove("main-header--nojs");
+}
 
-let toggle = document.querySelector('.site-header__toggle');
-let smenu = document.querySelector('.site-header__menu');
+let toggle = document.querySelector(".site-header__toggle");
+let smenu = document.querySelector(".site-header__menu");
+let sheader = document.querySelector(".site-header");
 
-toggle.addEventListener('click', () => {
-  if (smenu.classList.contains('site-header__menu-closed') == true) {
-    smenu.classList.remove('site-header__menu-closed');
-  } else {
-    smenu.classList.add('site-header__menu-closed');
-  }
-})
+if (sheader) {
+  sheader.classList.remove("site-header--nojs");
+}
+
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    if (smenu.classList.contains("site-header__menu-closed") == true) {
+      smenu.classList.remove("site-header__menu-closed");
+    } else {
+      smenu.classList.add("site-header__menu-closed");
+    }
+  });
+}
